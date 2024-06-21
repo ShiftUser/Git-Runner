85966c47f0c429d31fdb85c62e4f2f12d0fa2f5a
supattra789 <supattraboonchalee9@gmail.com>
2024-06-21 16:06:38 -0700

    edit

--- app/scripts/install-dependencies.ps1
+++ app/scripts/install-dependencies.ps1
@@ -2,15 +2,19 @@
 
 Write-Output "Installing NuGet packages..."
 
-dotnet add $env:Test_Project_Path package Appium.WebDriver
-dotnet add $env:Test_Project_Path package Coverlet.Collector
-dotnet add $env:Test_Project_Path package ExtentReports.Core
-dotnet add $env:Test_Project_Path package FluentAssertions
-dotnet add $env:Test_Project_Path package Microsoft.Extensions.Configuration.EnvironmentVariables
-dotnet add $env:Test_Project_Path package Microsoft.Extensions.Configuration.Json
-dotnet add $env:Test_Project_Path package Microsoft.NET.Test.Sdk
-dotnet add $env:Test_Project_Path package NUnit
-dotnet add $env:Test_Project_Path package NUnit.Analyzers
-dotnet add $env:Test_Project_Path package NUnit3TestAdapter
+dotnet add $env:Test_Project_Path package Appium.WebDriver --version 5.0.0
+dotnet add $env:Test_Project_Path package Selenium.WebDriver --version 4.21.0
+dotnet add $env:Test_Project_Path package Coverlet.Collector --version 3.0.3
+dotnet add $env:Test_Project_Path package ExtentReports.Core --version 1.0.2
+dotnet add $env:Test_Project_Path package FluentAssertions --version 6.2.0
+dotnet add $env:Test_Project_Path package Microsoft.Extensions.Configuration.EnvironmentVariables --version 8.0.0
+dotnet add $env:Test_Project_Path package Microsoft.Extensions.Configuration.Json --version 8.0.0
+dotnet add $env:Test_Project_Path package Microsoft.NET.Test.Sdk --version 17.0.0
+dotnet add $env:Test_Project_Path package NUnit --version 3.13.2
+dotnet add $env:Test_Project_Path package NUnit.Analyzers --version 3.2.0
+dotnet add $env:Test_Project_Path package NUnit3TestAdapter --version 4.0.0
+dotnet add $env:Test_Project_Path package Selenium.Support --version 4.21.0
 
 Write-Output "NuGet packages installation completed."
+
+
